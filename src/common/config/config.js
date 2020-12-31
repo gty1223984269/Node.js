2b86832927b2871dfa2d1d6c760daf9659eacb70
unknown <1223984269@qq.com>
2020-12-31 15:47:56 +0800

    0

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   default_module: 'api',
   weixin: {
-    appid: '', // 小程序 appid
-    secret: '', // 小程序密钥
+    appid: 'wx3123049bd0166066', // 小程序 appid
+    secret: 'd4d1863afdcc0da7194fb9bf3dfc2eb8', // 小程序密钥
     mch_id: '', // 商户帐号ID
     partner_key: '', // 微信支付密钥
     notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
