f410173a81af3c5e6e5b2a04980e7445dde57a5b
gty1223984269 <1223984269@qq.com>
2021-01-16 20:33:47 +0800

    增加菜单接口

--- src/api/controller/backEnd.js
+++ src/api/controller/backEnd.js
@@ -1,11 +1,30 @@
 const Base = require('./base.js');
 
+function generateTreeMenu(arr, pid = 0) {
+  const temp = [];
+  for (const item of arr) {
+    if (item.parentId === pid) {
+      item.children = generateTreeMenu(arr, item.id);
+      temp.push(item);
+    }
+  }
+  return temp;
+}
 module.exports = class extends Base {
-  async indexAction() {
-
-    const  menuList = await this.model('bk_menu').select();
+  /**
+   * 获取sku信息，用于购物车编辑时选择规格
+   * @returns {Promise.<Promise|PreventPromise|void>}
+   */
+  async menuListAction() {
+    const arr = await this.model('bk_menu').select();
+    const menuList = generateTreeMenu(arr,0);
     return this.success({
       menuList: menuList
     });
   }
+
+  
+
 };
+
+
